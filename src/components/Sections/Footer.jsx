5e60db5499cd0acb2523b9e55b4d4b9c1427d917
nikhilpant00 <105990262+nikhilpant00@users.noreply.github.com>
2023-04-11 12:55:15 +0530

    add refund policy

--- src/components/Sections/Footer.jsx
+++ src/components/Sections/Footer.jsx
@@ -4,6 +4,7 @@ import { Link as ScrollLink } from "react-scroll";
 import { Link } from "react-router-dom";
 // Assets
 import LogoImg from "../../assets/svg/Logo";
+// import { AiOutlineMail } from "react-icons/ai";
 
 export default function Contact(props) {
 
@@ -30,6 +31,14 @@ export default function Contact(props) {
                 <LogoImg />
               </ScrollLink>
               <div style={{ color: "white" }}>
+                <h5 style={{ marginBottom: "10px" }}>Contact Us</h5>
+                <p>
+                  <a style={{color:'white', pointer:'cursor'}} href="mailto:info@ocpl.tech">info@ocpl.tech</a>
+                </p>
+                <p>+91 9555-555-801</p>
+                <p>Karol Bagh, New Delhi</p>
+              </div>
+              <div style={{ color: "white" }}>
                 <h5 style={{ marginBottom: "10px" }}>Legal</h5>
                 <Link to="/terms" style={{ color: "white", pointer: "cursor" }}>
                   <p>Terms and Conditions</p>
@@ -40,7 +49,14 @@ export default function Contact(props) {
                 >
                   <p>Privacy Policy</p>
                 </Link>
+                <Link
+                  to="/refund"
+                  style={{ color: "white", pointer: "cursor" }}
+                >
+                  <p>Cancellation and Refund Policy</p>
+                </Link>
               </div>
+
               <ScrollLink
                 className="whiteColor animate pointer "
                 to={props.footer ? props.footer : "home"}
